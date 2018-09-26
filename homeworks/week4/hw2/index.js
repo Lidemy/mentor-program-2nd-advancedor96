3297f4e1403786e1bb773c6ac8764231435f1116
丁丁 <advancedor96@gmail.com>
2018-09-26 23:26:17 +0800

    淚

--- homeworks/week4/hw2/index.js
+++ homeworks/week4/hw2/index.js
@@ -1,29 +1,73 @@
 let email = document.querySelector('#email');
 email.addEventListener('blur', function(e){
-  if(email.value.length ===0 || !validateEmail(email.value)){
-    document.querySelector('#emailSection').style.backgroundColor='pink'
-    document.querySelector('#emailHint').innerText = '請輸入有效的電子郵件地址'
-  }else{
-    document.querySelector('#emailSection').style.backgroundColor='rgba(0,0,0,0)'
-    document.querySelector('#emailHint').innerText = ''
-  }  
+  checkEmail();
 });
+
+
+let nickname = document.querySelector('#nickname');
+nickname.addEventListener('blur', function(e){
+  check('#nickname');
+});
+
+let type = document.querySelector('.type');
+
+// 現在的職業
+let work = document.querySelector('#work');
+work.addEventListener('blur', function(e){
+  check('#work'); 
+});
+
+// 是否有程式相關背景
+let relative = document.querySelector('#relative');
+relative.addEventListener('blur', function(e){
+  check('#relative');
+});
+
+
+let btn = document.querySelector('.submit');
+btn.addEventListener('click', function(){
+  let isGood = true;
+  if( checkEmail() &&  check('#nickname') && check('#work') && check('#relative') ){
+    if ( document.querySelectorAll('.type:checked').length === 0){
+      document.querySelector('.type').parentNode.style.backgroundColor='pink'
+    }else{
+      document.querySelector('.type').parentNode.style.backgroundColor='rgba(0,0,0,0)'
+      alert('表單送出，資料請看console')
+    }
+  }
+
+})
 
 function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
-let nickname = document.querySelector('#nickname');
-nickname.addEventListener('blur', function(e){
-  if(nickname.value.length ===0){
-    document.querySelector('#nicknameSection').style.backgroundColor='pink'
-    document.querySelector('#nicknameHint').innerText = '這是必填問題'
+function check(selector){
+  let node = document.querySelector(selector);
+  if(node.value.length === 0){
+    node.parentNode.style.backgroundColor='pink'
+    node.nextElementSibling.innerText = '這是必填問題'
+    return false;
   }else{
-    document.querySelector('#nicknameSection').style.backgroundColor='rgba(0,0,0,0)'
-    document.querySelector('#nicknameHint').innerText = ''
-  }  
-});
+    node.parentNode.style.backgroundColor='rgba(0,0,0,0)'
+    node.nextElementSibling.innerText = ''
+    return true;
+  }
+}
+
+function checkEmail(){
+  let email = document.querySelector('#email');
+  if(email.value.length === 0 || !validateEmail(email.value)){
+    email.parentNode.style.backgroundColor='pink'
+    email.nextElementSibling.innerText = '請輸入有效的電子郵件地址'
+    return false;
+  }else{
+    email.parentNode.style.backgroundColor='rgba(0,0,0,0)'
+    email.nextElementSibling.innerText = ''
+    return true;
+  }
+}
 
 
 
